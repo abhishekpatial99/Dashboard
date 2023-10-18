4e4f3cbd664b2fc8713ba12eea3875081fc04348
abhishekpatial99 <145443410+abhishekpatial99@users.noreply.github.com>
2023-10-18 12:17:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+Morris.Donut({
+    element: 'donut',
+    data: [
+      {label: "65% Total new customers", value: 65},
+      {label: "25% New customer", value: 25},
+      {label: "10%Customers", value: 10} ]
+   
+  });
+//   Morris.Bar({
+//     element: 'bar',
+//     data: [
+//       { y: 'Jan', a:'10'},
+//       { y: 'Feb', a:"2"},
+//       { y: 'Mar', a:"6"},
+//       { y: 'Apr', a:'8'},
+//       { y: 'Jun',a:"8"},
+//       { y: 'Jul',a:"10"},
+//       { y: 'Aug',a:"8"},
+//       { y: 'Sept',a:"8"},
+//       { y: 'Oct',a:"8"},
+//       { y: 'Nov',a:"9"},
+//       { y: 'Dec',a:"7"}
+//     ],
+//     xkey: 'y',
+//     ykeys: ['a'],
+//     labels: ['Series A']
+//   });
